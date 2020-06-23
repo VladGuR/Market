0e4daffb456bcd7880de33d4b008a9ae403a2481
Владик <lotarev1999@inbox.ru>
2020-06-23 19:42:08 +0500

    n

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function changesOnNewDiv(){
-    document.querySelectorAll(".hover_block").style.display = 'flex';
+    document.querySelector(".hover_block").style.display = 'flex';
 }
 
 function changesOnOldDiv(){
-    document.querySelectorAll(".hover_block").style.display = 'none';
+    document.querySelector(".hover_block").style.display = 'none';
 }
 
