2c93a6f15f4fcd3b3b66e7cb9dc0e52dde96b886
Владик <lotarev1999@inbox.ru>
2020-06-23 18:46:47 +0500

    Js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function changesOnNewDiv(){
+    document.querySelectorAll(".hover_block").style.display = 'flex';
+}
+
+function changesOnOldDiv(){
+    document.querySelectorAll(".hover_block").style.display = 'none';
+}
+
