e8512292df55bd49a10e7de50e1586b124091124
Владик <lotarev1999@inbox.ru>
2020-06-28 11:08:01 +0500

    g

--- js/script.js
+++ js/script.js
@@ -1,8 +1,36 @@
+function showMenuLinks(){
+    document.querySelector(".block_menu_content").style.display = 'flex';
+    document.querySelector(".content_links").style.display = 'flex';
+
+}
+
+function closeMenuLinks(){
+    document.querySelector(".block_menu_content").style.display = 'none';
+    document.querySelector(".content_links").style.display = 'none';
+}
+
 function changesOnNewDiv(){
-    document.querySelector(".hover_block").style.display = 'flex';
+    document.querySelector(".hover_block_film").style.display = 'flex';
+    document.querySelector(".hover_block_serial").style.display = 'flex';
+    document.querySelector(".hover_block_anime").style.display = 'flex';
 }
 
 function changesOnOldDiv(){
-    document.querySelector(".hover_block").style.display = 'none';
+    document.querySelector(".hover_block_film").style.display = 'none';
+    document.querySelector(".hover_block_serial").style.display = 'none';
+    document.querySelector(".hover_block_anime").style.display = 'none';
 }
+
+function showBlockUserContent(){
+    document.querySelector(".block_with_user_content").style.display = 'flex';
+}
+
+function closeBlockUserContent(){
+    document.querySelector(".block_with_user_content").style.display = 'none';
+}
+
+let newFilms = document.querySelector(".new_films");
+let imgNamedFilms = document.querySelector(".img_named_films");
+let imgSlider = document.getElementById("imgSlider");
+let namedFilm = document.querySelector(".named_film");
 
